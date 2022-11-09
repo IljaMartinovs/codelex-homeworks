d87d9bc029bf6f257fc46bf2816cd6df8033cd8a
Iļja Martinovs <martinovsilja@gmail.com>
2022-11-09 21:44:15 +0200

    Added

---
+++ 4.Classes/11 Bank Account.php
@@ -0,0 +1,50 @@
+<?php
+
+class Account
+{
+    private string $name;
+    private float $balance;
+
+    public function __construct(string $name, float $balance)
+    {
+        $this->name = $name;
+        $this->balance = $balance;
+    }
+
+    public function Display(): string
+    {
+        return $this->name . " | ".number_format($this->balance,2) . PHP_EOL;
+    }
+
+    public function withdrawal(float $money): float
+    {
+        return $this->balance -= $money;
+    }
+
+    public function deposit(float $money): float
+    {
+        return $this->balance += $money;
+    }
+
+    public static function transfer(Account $from, Account $to, float $howMuch): float
+    {
+        $from->balance -= $howMuch;
+        return $to->balance += $howMuch;
+    }
+}
+$Matt = new Account( "Matt's account" , 100);
+$My = new Account("My account", 0);
+$Matt->withdrawal(100);
+$My->deposit(100);
+echo $Matt->Display();
+echo $My->Display() . "====================================" . PHP_EOL;
+
+$A = new Account("Matt's account", 100);
+$B = new Account("My account", 0);
+$C = new Account("C account", 0);
+Account::transfer($A,$B,50);
+Account::transfer($B,$C,25);
+echo "After transfer \n";
+echo "Matt's account balance is now: ". $A->Display();
+echo "My account balance is now: ". $B->Display();
+echo "C account balance is now: ". $C->Display();
